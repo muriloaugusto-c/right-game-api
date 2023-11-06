182e79fdce13c16f3e5e63dde477aa56ef0da673
muriloaugusto-c <muriloaugustoc@hotmail.com>
2023-11-05 21:03:13 -0300

    Correção de erros

--- app/Services/SportsCourts/DuplicateNameService.ts
+++ app/Services/SportsCourts/DuplicateNameService.ts
@@ -5,8 +5,9 @@ export default class DuplicateNameService {
   public async nameDuplicate(name: string, sportsCenterId: number): Promise<boolean> {
     const sportsCourt = await SportsCourt.query()
       .where('sports_center_id', sportsCenterId)
-      .where('name', 'like', name)
+      .andWhere('name', 'LIKE', '%' + name + '%')
+      .first()
     if (sportsCourt) throw new BadRequestException('Name is already in use', 409)
-    else return true
+    return true
   }
 }
