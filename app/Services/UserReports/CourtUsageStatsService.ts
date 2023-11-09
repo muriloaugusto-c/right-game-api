1f45a0195b584c864ae5176e487b546730f14cbe
muriloaugusto-c <muriloaugustoc@hotmail.com>
2023-11-08 23:13:06 -0300

    correção reports

--- app/Services/UserReports/CourtUsageStatsService.ts
+++ app/Services/UserReports/CourtUsageStatsService.ts
@@ -21,30 +21,41 @@ export default class CourtUsageStatsService {
 
     const monthlyTotals: MonthlyTotal[] = []
 
+    // Cria um objeto Map para rastrear os meses e suas contagens
+    const monthlyMap = new Map<number, MonthlyTotal>()
+
+    // Itera sobre as reservas aceitas
     acceptedReservations.forEach((reservation: Reservation) => {
       const startTime = new Date(reservation.startTime.toString())
       const month = startTime.getMonth() + 1
 
-      const existingTotal = monthlyTotals.find((total) => total.month === month)
-
-      if (existingTotal) {
-        existingTotal.completedReservations += 1
+      // Se o mês já existe no Map, incrementa a contagem
+      if (monthlyMap.has(month)) {
+        const existingTotal = monthlyMap.get(month)
+        if (existingTotal) {
+          existingTotal.completedReservations += 1
+        }
       } else {
-        monthlyTotals.push({ month, completedReservations: 1 })
+        // Se o mês não existe, cria um novo objeto MonthlyTotal
+        monthlyMap.set(month, { month, completedReservations: 1 })
       }
     })
 
-    // Adiciona os meses faltantes com zero reservas concluídas
+    // Preenche os meses ausentes com contagem zero
     for (let i = 1; i <= 12; i++) {
-      const existingTotal = monthlyTotals.find((total) => total.month === i)
-      if (!existingTotal) {
-        monthlyTotals.push({ month: i, completedReservations: 0 })
+      if (!monthlyMap.has(i)) {
+        monthlyMap.set(i, { month: i, completedReservations: 0 })
       }
     }
 
     // Ordena os resultados por mês
+    monthlyMap.forEach((value) => {
+      monthlyTotals.push(value)
+    })
+
     monthlyTotals.sort((a, b) => a.month - b.month)
 
-    return monthlyTotals
+    // Retorna somente os 12 meses
+    return monthlyTotals.slice(0, 12)
   }
 }
