68539993110e03ccfaeb8afd3b40ba51d0924373
muriloaugusto-c <muriloaugustoc@hotmail.com>
2023-11-08 23:32:04 -0300

    correção reports

--- app/Services/UserReports/CourtUsageStatsService.ts
+++ app/Services/UserReports/CourtUsageStatsService.ts
@@ -19,43 +19,21 @@ export default class CourtUsageStatsService {
       .where('user_id', userId)
       .andWhere('status', 'COMPLETED')
 
-    const monthlyTotals: MonthlyTotal[] = []
+    const monthlyTotals: MonthlyTotal[] = Array.from({ length: 12 }, (_, i) => ({
+      month: i + 1,
+      completedReservations: 0,
+    }))
 
-    // Cria um objeto Map para rastrear os meses e suas contagens
-    const monthlyMap = new Map<number, MonthlyTotal>()
-
-    // Itera sobre as reservas aceitas
     acceptedReservations.forEach((reservation: Reservation) => {
       const startTime = new Date(reservation.startTime.toString())
       const month = startTime.getMonth() + 1
 
-      // Se o mês já existe no Map, incrementa a contagem
-      if (monthlyMap.has(month)) {
-        const existingTotal = monthlyMap.get(month)
-        if (existingTotal) {
-          existingTotal.completedReservations += 1
-        }
-      } else {
-        // Se o mês não existe, cria um novo objeto MonthlyTotal
-        monthlyMap.set(month, { month, completedReservations: 1 })
+      const monthIndex = month - 1
+      if (monthIndex >= 0 && monthIndex < 12) {
+        monthlyTotals[monthIndex].completedReservations += 1
       }
     })
 
-    // Preenche os meses ausentes com contagem zero
-    for (let i = 1; i <= 12; i++) {
-      if (!monthlyMap.has(i)) {
-        monthlyMap.set(i, { month: i, completedReservations: 0 })
-      }
-    }
-
-    // Ordena os resultados por mês
-    monthlyMap.forEach((value) => {
-      monthlyTotals.push(value)
-    })
-
-    monthlyTotals.sort((a, b) => a.month - b.month)
-
-    // Retorna somente os 12 meses
-    return monthlyTotals.slice(0, 12)
+    return monthlyTotals
   }
 }
